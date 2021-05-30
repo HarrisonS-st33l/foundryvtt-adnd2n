3ba8c100b78377e632494220f9c9afac2dc53980
HarrisonS-st33l <harrisons.developer@gmail.com>
2021-05-31 00:26:06 +0100

    finished spell-card and spell-sheet styling

--- module/config.js
+++ module/config.js
@@ -14,6 +14,23 @@ adnd2n.sizes = {
 
 adnd2n.crits = {
     1 : "adnd2n.crits.none",
-    2: "adnd2n.crits.single",
-    3: "adnd2n.crits.double"
+    2 : "adnd2n.crits.single",
+    3 : "adnd2n.crits.double"
+}
+
+adnd2n.spellRarities = {
+    common : "adnd2n.spells.rarities.common",
+    uncommon : "adnd2n.spells.rarities.uncommon",
+    rare : "adnd2n.spells.rarities.rare"
+}
+
+adnd2n.spellSchools = {
+    invocation: "adnd2n.spells.schools.invocation",
+    enchantment: "adnd2n.spells.schools.enchantment",
+    conjuration: "adnd2n.spells.schools.conjuration",
+    abjuration: "adnd2n.spells.schools.abjuration",
+    necromancy: "adnd2n.spells.schools.necromancy",
+    illusion: "adnd2n.spells.schools.illusion",
+    alteration: "adnd2n.spells.schools.alteration",
+    divination: "adnd2n.spells.schools.divination"
 }
